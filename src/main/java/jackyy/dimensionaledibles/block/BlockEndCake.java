752bda928ec82339c78e49638f3fa6e8df4c61df
Turkey2349 <turkey2349@gmail.com>
2019-01-14 11:38:30 -0500

    Fixed a bug with cakes teleporting dimensions as well Players now teleport to the correct end spawn point when first using the end cake and not having previously been to the end

--- src/main/java/jackyy/dimensionaledibles/block/BlockEndCake.java
+++ src/main/java/jackyy/dimensionaledibles/block/BlockEndCake.java
@@ -25,77 +25,79 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class BlockEndCake extends BlockCakeBase implements ITileEntityProvider {
 
     public BlockEndCake() {
-        super();
-        setRegistryName(DimensionalEdibles.MODID + ":end_cake");
-        setTranslationKey(DimensionalEdibles.MODID + ".end_cake");
+	super();
+	setRegistryName(DimensionalEdibles.MODID + ":end_cake");
+	setTranslationKey(DimensionalEdibles.MODID + ".end_cake");
     }
 
     @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-        int meta = getMetaFromState(world.getBlockState(pos)) - 1;
-        ItemStack stack = player.getHeldItem(hand);
-        if (player.capabilities.isCreativeMode || meta < 0) {
-            meta = 0;
-        }
-        if (!stack.isEmpty() && stack.getItem() == Item.REGISTRY.getObject(new ResourceLocation(ModConfig.tweaks.endCake.fuel))) {
-            world.setBlockState(pos, getStateFromMeta(meta), 2);
-            if (!player.capabilities.isCreativeMode) {
-                stack.shrink(1);
-            }
-            return true;
-        } else {
-            if (world.provider.getDimension() != 1) {
-                if (!world.isRemote) {
-                    if (player.capabilities.isCreativeMode) {
-                        teleportPlayer(world, player);
-                    } else {
-                        consumeCake(world, pos, player);
-                    }
-                }
-            }
-        }
-        return true;
+	if (world.isRemote) {
+	    return false;
+	}
+
+	int meta = getMetaFromState(world.getBlockState(pos)) - 1;
+	ItemStack stack = player.getHeldItem(hand);
+	if (player.capabilities.isCreativeMode || meta < 0) {
+	    meta = 0;
+	}
+	if (!stack.isEmpty() && stack.getItem() == Item.REGISTRY.getObject(new ResourceLocation(ModConfig.tweaks.endCake.fuel))) {
+	    world.setBlockState(pos, getStateFromMeta(meta), 2);
+	    if (!player.capabilities.isCreativeMode) {
+		stack.shrink(1);
+	    }
+	    return true;
+	} else {
+	    if (world.provider.getDimension() != 1) {
+		if (player.capabilities.isCreativeMode) {
+		    teleportPlayer(world, player);
+		} else {
+		    consumeCake(world, pos, player);
+		}
+	    }
+	}
+	return true;
     }
 
     private void teleportPlayer(World world, EntityPlayer player) {
-        EntityPlayerMP playerMP = (EntityPlayerMP) player;
-        BlockPos coords;
-        if (ModConfig.tweaks.endCake.useCustomCoords) {
-            coords = new BlockPos(ModConfig.tweaks.endCake.customCoords.x, ModConfig.tweaks.endCake.customCoords.y, ModConfig.tweaks.endCake.customCoords.z);
-        } else {
-            coords = TeleporterHandler.getDimPos(playerMP, 1, player.getPosition());
-        }
-        TeleporterHandler.updateDimPos(playerMP, world.provider.getDimension(), player.getPosition());
-        TeleporterHandler.teleport(playerMP, 1, coords.getX(), coords.getY(), coords.getZ(), playerMP.server.getPlayerList());
+	EntityPlayerMP playerMP = (EntityPlayerMP) player;
+	BlockPos coords;
+	if (ModConfig.tweaks.endCake.useCustomCoords) {
+	    coords = new BlockPos(ModConfig.tweaks.endCake.customCoords.x, ModConfig.tweaks.endCake.customCoords.y, ModConfig.tweaks.endCake.customCoords.z);
+	} else {
+	    coords = TeleporterHandler.getDimPos(playerMP, 1, player.getPosition());
+	}
+	TeleporterHandler.updateDimPos(playerMP, world.provider.getDimension(), player.getPosition());
+	TeleporterHandler.teleport(playerMP, 1, coords.getX(), coords.getY(), coords.getZ(), playerMP.server.getPlayerList());
     }
 
     private void consumeCake(World world, BlockPos pos, EntityPlayer player) {
-        if (player.canEat(true)) {
-            int l = world.getBlockState(pos).getValue(BITES);
+	if (player.canEat(true)) {
+	    int l = world.getBlockState(pos).getValue(BITES);
 
-            if (l < 6) {
-                player.getFoodStats().addStats(2, 0.1F);
-                world.setBlockState(pos, world.getBlockState(pos).withProperty(BITES, l + 1), 3);
-                teleportPlayer(world, player);
-            }
-        }
+	    if (l < 6) {
+		player.getFoodStats().addStats(2, 0.1F);
+		world.setBlockState(pos, world.getBlockState(pos).withProperty(BITES, l + 1), 3);
+		teleportPlayer(world, player);
+	    }
+	}
     }
 
     @Override
     public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return ModConfig.tweaks.endCake.preFueled ? getStateFromMeta(0) : getStateFromMeta(6);
+	return ModConfig.tweaks.endCake.preFueled ? getStateFromMeta(0) : getStateFromMeta(6);
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public void getSubBlocks(CreativeTabs tab, NonNullList<ItemStack> list) {
-        if (ModConfig.general.endCake)
-            list.add(new ItemStack(this));
+	if (ModConfig.general.endCake)
+	    list.add(new ItemStack(this));
     }
 
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
-        return new TileDimensionCake(1, "End");
+	return new TileDimensionCake(1, "End");
     }
 
 }
--- src/main/java/jackyy/dimensionaledibles/block/BlockOverworldCake.java
+++ src/main/java/jackyy/dimensionaledibles/block/BlockOverworldCake.java
@@ -25,77 +25,80 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class BlockOverworldCake extends BlockCakeBase implements ITileEntityProvider {
 
     public BlockOverworldCake() {
-        super();
-        setRegistryName(DimensionalEdibles.MODID + ":overworld_cake");
-        setTranslationKey(DimensionalEdibles.MODID + ".overworld_cake");
+	super();
+	setRegistryName(DimensionalEdibles.MODID + ":overworld_cake");
+	setTranslationKey(DimensionalEdibles.MODID + ".overworld_cake");
     }
 
     @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-        int meta = getMetaFromState(world.getBlockState(pos)) - 1;
-        ItemStack stack = player.getHeldItem(hand);
-        if (player.capabilities.isCreativeMode || meta < 0) {
-            meta = 0;
-        }
-        if (!stack.isEmpty() && stack.getItem() == Item.REGISTRY.getObject(new ResourceLocation(ModConfig.tweaks.overworldCake.fuel))) {
-            world.setBlockState(pos, getStateFromMeta(meta), 2);
-            if (!player.capabilities.isCreativeMode) {
-                stack.shrink(1);
-            }
-            return true;
-        } else {
-            if (world.provider.getDimension() != 0) {
-                if (!world.isRemote) {
-                    if (player.capabilities.isCreativeMode) {
-                        teleportPlayer(world, player);
-                    } else {
-                        consumeCake(world, pos, player);
-                    }
-                }
-            }
-        }
-        return true;
+	if (world.isRemote) {
+	    return false;
+	}
+
+	int meta = getMetaFromState(world.getBlockState(pos)) - 1;
+	ItemStack stack = player.getHeldItem(hand);
+	if (player.capabilities.isCreativeMode || meta < 0) {
+	    meta = 0;
+	}
+	if (!stack.isEmpty() && stack.getItem() == Item.REGISTRY.getObject(new ResourceLocation(ModConfig.tweaks.overworldCake.fuel))) {
+	    world.setBlockState(pos, getStateFromMeta(meta), 2);
+	    if (!player.capabilities.isCreativeMode) {
+		stack.shrink(1);
+	    }
+	    return true;
+	} else {
+	    if (world.provider.getDimension() != 0) {
+		if (player.capabilities.isCreativeMode) {
+		    teleportPlayer(world, player);
+		} else {
+		    consumeCake(world, pos, player);
+		}
+	    }
+	}
+	return true;
     }
 
     private void teleportPlayer(World world, EntityPlayer player) {
-        EntityPlayerMP playerMP = (EntityPlayerMP) player;
-        BlockPos coords;
-        if (ModConfig.tweaks.overworldCake.useCustomCoords) {
-            coords = new BlockPos(ModConfig.tweaks.overworldCake.customCoords.x, ModConfig.tweaks.overworldCake.customCoords.y, ModConfig.tweaks.overworldCake.customCoords.z);
-        } else {
-            coords = TeleporterHandler.getDimPos(playerMP, 0, player.getPosition());
-        }
-        TeleporterHandler.updateDimPos(playerMP, world.provider.getDimension(), player.getPosition());
-        TeleporterHandler.teleport(playerMP, 0, coords.getX(), coords.getY(), coords.getZ(), playerMP.server.getPlayerList());
+	EntityPlayerMP playerMP = (EntityPlayerMP) player;
+	BlockPos coords;
+	if (ModConfig.tweaks.overworldCake.useCustomCoords) {
+	    coords = new BlockPos(ModConfig.tweaks.overworldCake.customCoords.x, ModConfig.tweaks.overworldCake.customCoords.y, ModConfig.tweaks.overworldCake.customCoords.z);
+	} else {
+	    coords = TeleporterHandler.getDimPos(playerMP, 0, player.getPosition());
+	}
+	TeleporterHandler.updateDimPos(playerMP, world.provider.getDimension(), player.getPosition());
+	TeleporterHandler.teleport(playerMP, 0, coords.getX(), coords.getY(), coords.getZ(), playerMP.server.getPlayerList());
     }
 
     private void consumeCake(World world, BlockPos pos, EntityPlayer player) {
-        if (player.canEat(true)) {
-            int l = world.getBlockState(pos).getValue(BITES);
+	if (player.canEat(true)) {
+	    System.out.println(world + " " + player.getEntityWorld());
+	    int l = world.getBlockState(pos).getValue(BITES);
 
-            if (l < 6) {
-                player.getFoodStats().addStats(2, 0.1F);
-                world.setBlockState(pos, world.getBlockState(pos).withProperty(BITES, l + 1), 3);
-                teleportPlayer(world, player);
-            }
-        }
+	    if (l < 6) {
+		player.getFoodStats().addStats(2, 0.1F);
+		world.setBlockState(pos, world.getBlockState(pos).withProperty(BITES, l + 1), 3);
+		teleportPlayer(world, player);
+	    }
+	}
     }
 
     @Override
     public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return ModConfig.tweaks.overworldCake.preFueled ? getStateFromMeta(0) : getStateFromMeta(6);
+	return ModConfig.tweaks.overworldCake.preFueled ? getStateFromMeta(0) : getStateFromMeta(6);
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public void getSubBlocks(CreativeTabs tab, NonNullList<ItemStack> list) {
-        if (ModConfig.general.overworldCake)
-            list.add(new ItemStack(this));
+	if (ModConfig.general.overworldCake)
+	    list.add(new ItemStack(this));
     }
 
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
-        return new TileDimensionCake(0, "Overworld");
+	return new TileDimensionCake(0, "Overworld");
     }
 
 }
